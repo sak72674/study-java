3cb0d036d977045b6040de8727555d765b932056
小田切彩織 <saori@saoris-MacBook-Air.local>
2020-11-14 17:04:48 +0900

    finish CH4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "小田切　彩織";
+		System.out.println(str);
+
+		str = "勉強中";
+		System.out.println(str);
+
+		final int LUCKY_NUM = 7;
+		System.out.println(LUCKY_NUM);
+	}
+}
