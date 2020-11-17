055495f37d7ab7ade9a6d4b92ed66256b597b95b
小田切彩織 <saori@saoris-MacBook-Air.local>
2020-11-17 17:44:41 +0900

    finish CH7

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long l = 9223372036854775807L;
+		System.out.println(l);
+		double d = 1.256892;
+		System.out.println(d);
+		boolean b = true;
+		System.out.println(b);
+
+		Integer i = 523;
+		System.out.println(i);
+		Float f = 1.2923f;
+		System.out.println(f);
+		Character c = 'あ';
+		System.out.println(c);
+
+		int[] array = { 5, 2, 3, 4, 6 };
+		System.out.println(array[3]);
+	}
+}
